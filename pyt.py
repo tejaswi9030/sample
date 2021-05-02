7b5e95a9de783e6c998496be4bd21d42ff3fdd35
Bandlamudi Tejaswi <tejaswibandlamudi222@gmail.com>
2021-05-03 00:01:47 +0530

    intial

---
+++ pyt.py
@@ -0,0 +1,29 @@
+# Program to check if a number is prime or not
+import sys
+
+import math
+num = 29
+print(sys.version)
+print(sys.executable)
+from os import rename
+# To take input from the user
+#num = int(input("Enter a number: "))
+
+# define a flag variable
+flag = False
+
+# prime numbers are greater than 1
+if num > 1:
+    # check for factors
+    for i in range(2, num):
+        if (num % i) == 0:
+            # if factor is found, set flag to True
+            flag = True
+            # break out of loop
+            break
+
+# check if flag is True
+if flag:
+    print(num, "is not a prime number")
+else:
+    print(num, "is a prime number")
